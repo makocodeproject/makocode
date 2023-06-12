901be667f46f97599ab15030242d81db9ac92bb6
Justin Bass <justinalanbass@gmail.com>
2023-06-11 20:44:53 -0700

    PCG TODO

--- src/makocode/encoder.hpp
+++ src/makocode/encoder.hpp
@@ -18,16 +18,16 @@ void encode(int height, int width) {
 
     ofstream file;
     file.open ("image.ppm");
-    int max_val = 255;
+    unsigned int max_val = 255;
 
     file << "P3\n" << width << "\n" << height << "\n" << max_val;
 
-    for (int i = 0; i < width * height * RGB_CHANNEL_COUNT; i++) {
+    for (unsigned int i = 0; i < width * height * RGB_CHANNEL_COUNT; i++) {
         if (i % RGB_CHANNEL_COUNT == 0) {
             file << "\n";
         }
 
-        file << int(rand_u32() % (max_val + 1)) << " ";
+        file << (unsigned int) (rand_u32() % (max_val + 1)) << " ";
     }
 
     file.close();
